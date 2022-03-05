8019936e1a3e4fd6b736b571b98821b09a840d40
carternadain <cnadain@gmail.com>
2022-03-04 20:50:47 -0700

    add comments and css

---
+++ assets/js/script.js
@@ -0,0 +1,72 @@
+var forecastWeather;
+var cityLat;
+var cityLong;
+var cityAPISearch = [];
+var citySearchString;
+var searchHistory = [];
+var dateFormatCurrent;
+var dateFormatForecast = [];
+var dtForecast;
+const searchButtonEl = $('#search');
+const cityInputEl = $('#city-search');
+
+
+// retrieve local storage
+
+
+
+
+
+// function to gather weather conditions based on the city lat and long provided from the geocoding api
+
+
+
+
+
+// function to gather city lat and long to use in the openweather api
+
+
+
+
+
+// function to execute when the search button is clicked
+
+
+
+
+// function to render current weather condition content
+
+// render current weather conditions in city searched
+
+
+
+
+
+
+
+// function to render the forecast content on the page
+// start loop at 1 as first value in daily array is the current date
+
+
+
+
+
+
+
+
+// function to populate search history below search bar
+
+
+
+
+
+
+
+
+// search button click functionality
+
+
+
+
+
+// search history button click functionality
