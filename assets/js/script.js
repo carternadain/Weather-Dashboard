b23794391555f4ac1b35b4fdbda06a67f5dc4093
carternadain <cnadain@gmail.com>
2022-04-30 17:01:42 -0700

    get single city weather to show

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,31 @@
 // const  = document.querySelector("#");
-// const  = document.querySelector("#");
-// const  = document.querySelector('#');
-// const  = document.querySelector('#');
-
+const fiveDayWeather = document.querySelector("#fivedayweather");
+const submitButton = document.querySelector('#submit');
+const citySearch = document.querySelector('#searchCity');
+const currentWeather = document.querySelector('#currentweather')
+const temp = document.querySelector('#temp')
+const humidity = document.querySelector('#humidity')
+const windspeed = document.querySelector('#windspeed')
 
 
 
 
 // current weather function for a specefic city (temp,wind,humid,UV index)
-fetch('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=ffdd7626b68a3f81a3df32c8ea82937a')
-.then(response => response.json())
-.then(data => {
+var singleCityWeather = function(city) {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=ffdd7626b68a3f81a3df32c8ea82937a')
+     .then(response => response.json())
+     .then(response => {
+     currentWeather.innerHTML += response.name
+     humidity.innerHTML += ' ' + response.main.humidity
+     windspeed.innerHTML += ' ' + response.wind.speed
+        console.log(response) 
+     }) 
+}
+        
+
 
 // .catch(err => alert("Wrong city name!"));
-})
+
 
 
 
@@ -22,21 +34,34 @@ fetch('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=ffdd7626b68
 
 
 // 5 day weather function for that city searched
-fetch('https://api.openweathermap.org/data/2.5/weather?q=miami&appid=ffdd7626b68a3f81a3df32c8ea82937a')
-.then(response => response.json())
-.then(data => {
+// var fiveDayWeather = function(city) {
+//  
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+city+'&units=imperial&appid=ffdd7626b68a3f81a3df32c8ea82937a')
+//      .then(response => response.json())
+//      .then(response => {
+//         // fivedayWeather.innerHTML += response.name
+//         // fivedayWeather.innerHTML += ' ' + response.main.temp
+//         // fivedayWeather.innerHTML += ' ' + response.main.humidity
+//         // fivedayWeather.innerHTML += ' ' + response.wind.speed
 
-// .catch(err => alert("Wrong city name!"));
-})
+//         console.log(response) 
+//      }) 
+// }
 
 
 
 
 
 // add eventlister to make search button work
-
+submitButton.addEventListener ('click',function(){
+    singleCityWeather (citySearch.value);
+    console.log(citySearch.value)
+})
 
 
 
 
 // local storage area for previous searched cities
+var saveSearch = function(){
+    localStorage.setItem("cities", JSON.stringify(cities));
+};
